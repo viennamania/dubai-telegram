8ffdb2c03a436134c2991c1745a7496131785714
seunghyonpark <creath.park@gmail.com>
2025-07-18 20:38:27 +0900

    feat: implement user management functions for userBananaBot API
    - Added UserProps and ResultProps interfaces for user data structure.
    - Implemented insertOne and insertOneVerified functions for user registration.
    - Created update functions for user details, avatar, seller status, and telegram ID.
    - Developed retrieval functions for user data by wallet address, telegram ID, nickname, and contract address.
    - Added pagination and filtering for fetching all users and specific user groups.
    - Implemented functions for managing user game money and settlement amounts.
    - Added functions to update user wallet addresses and contract addresses.
    - Implemented search functionality for users based on name and username.
    - Added error handling for user operations and ensured data integrity.

--- next-app/src/lib/api/telegram.ts
+++ next-app/src/lib/api/telegram.ts
@@ -863,6 +863,78 @@ export async function insertMessageByUseridAndStorecodeStrawberryBot(
 
 
 
+export async function insertMessageByUseridAndStorecodeBananaBot(
+    {
+        center,
+        category,
+        userid,
+        storecode,
+        message,
+    }
+    :
+    {
+        center: string,
+        category: string,
+        userid: string,
+        storecode: string,
+        message: string,
+    }
+) {
+
+    console.log("insertMessageByUseridAndStorecodeBananaBot", {
+        center,
+        category,
+        userid,
+        storecode,
+        message,
+    });
+
+    const client = await clientPromise;
+
+    const collectionTelegramMessages = client.db('dubai').collection('telegramMessages');
+
+    const user = await client.db('dubai').collection('usersBananaBot').findOne(
+        {
+            ////storecode: storecode,
+            nickname: userid,
+        },
+    );
+
+    if (user && user.telegramId) {
+
+        await collectionTelegramMessages.insertOne(
+            {
+                center,
+                category,
+                telegramId: user.telegramId,
+                message,
+            }
+        );
+
+        return {
+            result: "success",
+        };
+
+
+    } else {
+        console.log("user not found", userid, storecode);
+        return {
+            result: "user not found",
+        };
+    }
+
+
+}
+
+
+
+
+
+
+
+
+
+
 // getAllMessages
 export async function getAllLottoMessages(data: any) {
 
