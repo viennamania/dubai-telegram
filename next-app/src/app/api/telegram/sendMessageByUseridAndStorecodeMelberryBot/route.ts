3002bd055a3f3845d21dd7b62451c8109f894e87
seunghyonpark <creath.park@gmail.com>
2025-09-08 21:37:36 +0900

    kjdsa

---
+++ next-app/src/app/api/telegram/sendMessageByUseridAndStorecodeMelberryBot/route.ts
@@ -0,0 +1,36 @@
+import { NextResponse, type NextRequest } from "next/server";
+
+import {
+	insertMessageByUseridAndStorecodeMelberryBot,
+} from '@lib/api/telegram';
+
+
+export async function POST(request: NextRequest) {
+
+  const body = await request.json();
+
+  const {
+    center,
+    userid,
+    storecode,
+    message,
+  } = body;
+
+
+
+  const result = await insertMessageByUseridAndStorecodeMelberryBot({
+    center: center,
+    category: "wallet",
+    userid,
+    storecode,
+    message,
+  } );
+
+ 
+  return NextResponse.json({
+
+    result,
+    
+  });
+  
+}
--- next-app/src/lib/api/telegram.ts
+++ next-app/src/lib/api/telegram.ts
@@ -1390,6 +1390,61 @@ export async function insertMessageByUseridAndStorecodeLycheeBot(
 
 
 
+export async function insertMessageByUseridAndStorecodeMelberryBot(
+    {
+        center,
+        category,
+        userid,
+        storecode,
+        message,
+    }
+    :
+    {
+        center: string,
+        category: string,
+        userid: string,
+        storecode: string,
+        message: string,
+    }
+) {
+
+    const client = await clientPromise;
+
+    const collectionTelegramMessages = client.db('dubai').collection('telegramMessages');
+
+    const user = await client.db('dubai').collection('usersMelberryBot').findOne(
+        {
+            ////storecode: storecode,
+            nickname: userid,
+        },
+    );
+
+    if (user && user.telegramId) {
+
+        await collectionTelegramMessages.insertOne(
+            {
+                center,
+                category,
+                telegramId: user.telegramId,
+                message,
+            }
+        );
+
+        return {
+            result: "success",
+        };
+
+
+    } else {
+        console.log("user not found", userid, storecode);
+        return {
+            result: "user not found",
+        };
+    }
+
+
+}
+
 
 
 
